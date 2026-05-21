79c2cbc6bbfde093af12b6376a3987242eb805be
Devendra Pratap Singh <dpsingh936803@gmail.com>
2026-05-21 23:23:34 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 const express=require("express");
 const app=express();
 const {adminAuth}=require("./middlewares/auth");
+
 app.use("/admin",adminAuth)
 
+
 app.get("/admin/getAllData",(req,res)=>{
+    throw new Error("something went wrogng");
     res.send("All Data Send");
+    
 })
 app.delete("/admin/deleteUser",(req,res)=>{
     res.send("user delete sucessfully");
 })
 
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        console.log("Error handler");
+        res.status(400).send("something went wrong");
+    }
+})
+
 app.listen(3000,()=>{
+
     console.log("Server is listening on port 3000");
 })
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 const adminAuth=(req,res,next)=>{
     const token="xyz";
-    if(token!=="xyzx"){
+    if(token!=="xyz"){
         res.send("you are not a valid user");
     }else{
         next();
