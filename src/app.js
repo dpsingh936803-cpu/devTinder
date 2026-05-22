9b9b4a0217d9787824fdb7c396c122c4252d5abf
Devendra Pratap Singh <dpsingh936803@gmail.com>
2026-05-23 00:49:02 +0530

    database connected successfully and server start listening on port 7777

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
 const express=require("express");
+const connectDB=require("./config/database")
 const app=express();
-const {adminAuth}=require("./middlewares/auth");
-
-app.use("/admin",adminAuth)
-
-
-app.get("/admin/getAllData",(req,res)=>{
-    throw new Error("something went wrogng");
-    res.send("All Data Send");
-    
-})
-app.delete("/admin/deleteUser",(req,res)=>{
-    res.send("user delete sucessfully");
-})
-
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        console.log("Error handler");
-        res.status(400).send("something went wrong");
-    }
-})
-
-app.listen(3000,()=>{
+connectDB().then(()=>{
+    console.log("database established successfully");
+    app.listen(3000,()=>{
 
     console.log("Server is listening on port 3000");
 })
+}).catch((err)=>{
+    console.log("something went wrong");
+})
+app.get("/",(req,res)=>{
+    res.send("welcome to tinder .com");
+})
+
+
+
+
